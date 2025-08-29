09123d86a1a9c026b10890171cde6156f044edd5
Dmtejas <tejastejas0770@gmail.com>
2025-08-29 07:57:20 +0530

    Updating the time of the tutorial

--- src/CompanianObjectDemo.kt
+++ src/CompanianObjectDemo.kt
@@ -1,14 +1,17 @@
 fun main(args: Array<String>) {
-    val obj = CompanianObjectDemo()
-    println(obj.sum(2, 5))
+//    val obj = CompanianObjectDemo()
+//    println(obj.sum(2, 5))
+    val res: Int = CompanianObjectDemo.sum(2, 6)
+    println(res)
 }
 
 class CompanianObjectDemo() {
+    //It is an object where, It allows us to access the properties of the class without creating an object
 
-
-    fun sum(a: Int, b: Int): Int {
-        return a+b
+    companion object {
+        fun sum(a: Int, b: Int): Int {
+            return a+b
+        }
     }
-
 
 }
